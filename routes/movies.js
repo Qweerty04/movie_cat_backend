9bab9403f3171e05d2da24ca4c3c5da79c962af4
M. Sina Yaqubi <hello.cina@gmail.com>
2024-05-04 04:47:35 +0430

    working API

---
+++ routes/movies.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const Movie = require('../models/Movie');
+
+const router = express.Router();
+
+// Search Route
+router.get('/search', async (req, res) => {
+    try {
+        const query = req.query.q.trim(); // Get the search query from the request parameters
+
+        // Perform a search query in all fields of the movies
+        const movies = await Movie.find({
+            $or: [
+                { title: { $regex: query, $options: 'i' } },
+                { synopsis: { $regex: query, $options: 'i' } },
+                { plot: { $regex: query, $options: 'i' } }
+            ]
+        }).select('releaseDate title landscapeImage _id'); // Only select the necessary fields
+
+        // Return the search results as a JSON response
+        res.json(movies);
+    } catch (error) {
+        console.error('Error searching for movies:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+// Movie Details Route
+router.get('/:id', async (req, res) => {
+    try {
+        const movieId = req.params.id;
+
+        // Find the movie by ID
+        const movie = await Movie.findById(movieId).lean();
+
+        if (!movie) {
+            return res.status(404).json({ error: 'Movie not found' });
+        }
+
+        // Remove ID fields from the movie object
+        delete movie._id;
+        delete movie.__v;
+
+        // Return the movie information as a JSON response
+        res.json(movie);
+    } catch (error) {
+        console.error('Error retrieving movie details:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+module.exports = router;
